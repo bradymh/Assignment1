50bcad07b78fdfc1e8c07bde888c7a341956306b
Brady Hopkins <bradymh813@gmail.com>
2023-01-27 00:00:35 -0500

    Starting course class

--- Library.LMS/Models/Course.cs
+++ Library.LMS/Models/Course.cs
@@ -1,7 +1,23 @@
 ﻿namespace Library.LMS.Models
 {
-    public class Class1
+    public class Course
     {
+        private string _code;
+        public string Code { get { return _code; } set { _code = value; } }
+
+        private string _name;
+        public string Name { get { return _name;} set { _name = value; } }
+
+        private string _description;
+        public string Description { get { return _description;} set { _description = value; } }
+
+        List<Person> Roster = new List<Person>();
+
+        List<Assignment> Assignments = new List<Assignment>();
+
+        List<Module> Modules = new List<Module>();
+
+        public Course() { }
 
     }
 }
