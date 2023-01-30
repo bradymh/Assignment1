36acc10778a042beb653e07210f46ea5134e5012
Brady Hopkins <bradymh813@gmail.com>
2023-01-30 14:29:11 -0500

    removed tests

--- LMS/Program.cs
+++ LMS/Program.cs
@@ -9,24 +9,6 @@ namespace LMS
             List<Course> courses = new List<Course>();
             List<Person> students = new List<Person>();
 
-            //Person student = new Person();
-            //student.Name= "Test";
-            //student.Classification = "Junior";
-            //Assignment assignment1 = new Assignment();
-            //assignment1.DueDate = "1/2/22";
-            //assignment1.Name = "TestAssign";
-            //assignment1.TotalAvailablePoints = "100";
-            //assignment1.Description = "Description";
-
-
-
-            //student.AddGrade(assignment1, "100");
-
-            //Console.WriteLine(student);
-            //assignment1.Name= "NewName";
-
-            //Console.WriteLine(student);
-
             while (true)
             {
                 Console.WriteLine("1. Add a course");
